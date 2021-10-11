f6a27a09737aac9efa067d7f7e3cbfbcd1e84118
Jose Luis <wildchamo@gmail.com>
2021-10-11 16:30:34 -0500

    creación de nuevo componente para los botones del header 2

--- parcial/src/componentes/header.js
+++ parcial/src/componentes/header.js
@@ -1,2 +1,12 @@
-import react from "react";
+import React from "react";
+import '../estilos/header.css';
 
+const Header= function(){
+    return(
+        <header>
+            <img src="../images/sony_logo.svg"></img>
+        </header>
+        )
+}
+
+export default Header;
