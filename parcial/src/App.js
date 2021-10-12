5534f5e75225bbd5ccd1d91ab669b622792a51ef
Jose Luis <wildchamo@gmail.com>
2021-10-12 14:05:44 -0500

    base del footer

--- parcial/src/App.js
+++ parcial/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './componentes/header';
 import Header2 from './componentes/header2/header2';
 import Header3 from './componentes/header3/header3';
+import Footer from './componentes/footer.js';
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
       <Header/>
       <Header2/>
       <Header3/>
+
+
+      <Footer/>
 
     </div>
   );
---
+++ parcial/src/componentes/footer/footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import '../estilos/footer.css';
+import Section1 from './section1.js'
+import Section2 from './section2.js'
+import Section3 from './section3.js'
+
+const Footer= function(){
+    return(
+        <footer>
+            <Section1/>
+            <Settion2/>
+            <Section3/>
+        </footer>
+        )
+}
+
+export default Footer;
