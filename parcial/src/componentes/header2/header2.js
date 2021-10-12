8538fb96e5c1ea92ab540ba3bc20174dabb43ced
Jose Luis <wildchamo@gmail.com>
2021-10-12 11:00:59 -0500

    estilos para el header2

--- parcial/src/componentes/header2/header2.js
+++ parcial/src/componentes/header2/header2.js
@@ -1,10 +1,11 @@
 import React from "react";
 import NavCompartido1 from "./navCompartido1";
 import NavCompartido2 from "./navCompartido2";
+import '../../estilos/header2.css';
 
 const Header2= function(){
     return(
-        <div>
+        <div className="header2">
             <NavCompartido1/>
             <NavCompartido2/>
             
