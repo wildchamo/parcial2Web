e95e45d63829ed9404465b00e1ca39280657aab4
Jose Luis <wildchamo@gmail.com>
2021-10-12 11:15:46 -0500

    estilos al header 2

--- parcial/src/componentes/header2/navCompartido2.js
+++ parcial/src/componentes/header2/navCompartido2.js
@@ -1,10 +1,12 @@
 import React from "react";
 import BotonU from "./botonU";
 import IconLupa from "./IconLupa.js";
+import '../../estilos/NavCompartido2.css';
+
 
 const NavCompartido2= function(){
     return(
-        <div>
+        <div className="NavCompartido2">
             <BotonU/>
             <IconLupa/>
         </div>
