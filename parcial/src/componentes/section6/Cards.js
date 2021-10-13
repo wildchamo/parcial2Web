3e25df4672f5d581cbdf1958665d4a08e4ba3e10
Jose Luis <wildchamo@gmail.com>
2021-10-13 16:23:59 -0500

    section6 finalizada

--- parcial/src/componentes/section6/Cards.js
+++ parcial/src/componentes/section6/Cards.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "../../estilos/cards.css"
+import CardS6 from "./cards6.js"
+import card1 from "../../images/card1S6.webp";
+import card2 from "../../images/card2S6.webp";
+import card3 from "../../images/card3S6.webp";
+import card4 from "../../images/card4S6.webp";
+import card5 from "../../images/card5S6.webp";
+import card6 from "../../images/card6S6.webp";
 
 const Cards = function(){
     return (
         <div className="cardsS6">
-    
-            </div>  
+                <CardS6 source={{source:card1}}/>
+                <CardS6 source={{source:card2}}/>
+                <CardS6 source={{source:card3}}/>
+                <CardS6 source={{source:card4}}/>
+                <CardS6 source={{source:card5}}/>
+                <CardS6 source={{source:card6}}/>
+        </div>  
 
 )
 }
---
+++ parcial/src/componentes/section6/cards6.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../estilos/cardS6.css"
+
+const CardS6 = function(props){
+    const {source}=props.source;
+    return (
+        <img className="cardS6" src={source}>
+    
+        </img>  
+
+)
+}
+export default CardS6;
