b5acb7881479fbe1bf8c99d39c79e3d17e7cac60
Jose Luis <wildchamo@gmail.com>
2021-10-13 15:56:43 -0500

    botón y titulo de la section6 listos

--- parcial/src/componentes/section6/BotonS6.js
+++ parcial/src/componentes/section6/BotonS6.js
@@ -2,9 +2,9 @@ import React from "react";
 import "../../estilos/botonS6.css"
 const BotonS6 = function(){
     return(
-        <div className="botonS6">
+        <button className="botonS6">
             Mostrar más
-        </div>  
+        </button>  
         )
 }
 export default BotonS6;
