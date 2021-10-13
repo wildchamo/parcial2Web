9668bf2cb54ddac0126caf20392d66856b8a46ed
Jose Luis <wildchamo@gmail.com>
2021-10-13 16:35:39 -0500

    base de la section1

--- parcial/src/App.js
+++ parcial/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Header from './componentes/header';
 import Header2 from './componentes/header2/header2';
 import Header3 from './componentes/header3/header3';
+import Section1 from './componentes/section1/section1.js';
+// import Section5 from './componentes/section5/section5.js';
 import Section6 from './componentes/section6/section6.js';
 import Footer from './componentes/footer/footer.js';
 
@@ -11,6 +13,8 @@ function App() {
       <Header/>
       <Header2/>
       <Header3/>
+      <Section1/>
+
 
       <Section6/>
       <Footer/>
---
+++ parcial/src/componentes/section1/section1.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Cover from "../cover.js";
+import Info from "../info.js";
+import cover1 from "../../images/cover1.png";
+import logo1 from "../../images/logo1.png";
+
+ const Section1= function(){
+    return(
+        <section className="section1">
+      <Cover data={{logo:{logo1} ,cover:{cover1} }}/>
+      <Info data={{title:"Bienvenido a PlayStation Plus",info:"Potencia tu experiencia PlayStation con multijugador en línea, juegos mensuales, descuentos exclusivos y mucho más.", boton: "Más información" }}/>
+        </section>
+        )
+}
+
+export default Section6;
